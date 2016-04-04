220a8c07782cc7bc17d9266f7d6e16da493c25ba
Ammon Bartram <ammonbartram@gmail.com>
2016-04-04 16:58:58 -0700

    fix bug in http view when game is not active

--- lib/player.py
+++ lib/player.py
@@ -1,7 +1,8 @@
 class Player(object):
     def __init__(self, name, connection):
-        self.name = name 
+        self.name = name
         self.connection = connection
-    
+        self.piece = None
+
     def send_message(self, message):
         self.connection.transport.write('\n'.join(message) + '\n\n')
